02fa0992065393ab2b881deefcf314505140f7a2
Matias <matiastucci@gmail.com>
2016-01-30 04:14:30 +0100

    add anonymus function

--- app/controllers/config-controller.js
+++ app/controllers/config-controller.js
@@ -1,29 +1,31 @@
-'use strict';
-angular.module('subsApp')
-.controller('configCtrl',['$scope','$state','Config', function($scope,$state, Config){
+(function(){
+  'use strict';
+  angular.module('subsApp')
+  .controller('configCtrl',['$scope','$state','Config', function($scope,$state, Config){
 
-	var appConfig = Config.read();
+    var appConfig = Config.read();
 
-	$scope.languages = [
-    {name:'English', value:'eng'},
-    {name:'French', value:'fre'},
-    {name:'Spanish', value:'spa'},
-    {name:'Italian', value:'ita'},
-    {name:'German', value:'ger'},
-    {name:'Portuguese', value:'por'}
-  ];
+    $scope.languages = [
+      {name:'English', value:'eng'},
+      {name:'French', value:'fre'},
+      {name:'Spanish', value:'spa'},
+      {name:'Italian', value:'ita'},
+      {name:'German', value:'ger'},
+      {name:'Portuguese', value:'por'}
+    ];
 
-  $scope.subExtensions = [
-    {name:'.srt', value:'srt'},
-    {name:'.vtt', value:'vtt'}
-  ];
+    $scope.subExtensions = [
+      {name:'.srt', value:'srt'},
+      {name:'.vtt', value:'vtt'}
+    ];
 
-  $scope.language = appConfig.language;
-  $scope.subExtension = appConfig.subExtension;
+    $scope.language = appConfig.language;
+    $scope.subExtension = appConfig.subExtension;
 
-  $scope.save = function(){
-  	Config.save($scope.language,$scope.subExtension);
-  	$state.go('home');
-  };
+    $scope.save = function(){
+      Config.save($scope.language,$scope.subExtension);
+      $state.go('home');
+    };
 
-}]);
+  }]);
+})();
