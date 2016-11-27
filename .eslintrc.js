3a461658c630c4fc2f83c3ae8f4270c4ad9c7383
Matias Tucci <matiastucci@gmail.com>
2016-11-27 19:00:23 +0700

    allow console.log

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,7 @@ module.exports = {
     // allow debugger during development
     'no-restricted-syntax': process.env.NODE_ENV === 'production' ? 2 : 0,
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-console': 0,
     'func-names': 0,
     'semi': 0,
   }
