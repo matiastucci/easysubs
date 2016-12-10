d1be58488cf6cf0157444e474128681a3e05af01
Matias Tucci <matiastucci@gmail.com>
2016-12-11 00:53:15 +0700

    support external links

--- app/src/components/Info.vue
+++ app/src/components/Info.vue
@@ -18,17 +18,17 @@
   #info.padded-more
     h5 What's this?
     p Easysubs is an open source app that allows you to download subtitles from
-      a(href='http://www.opensubtitles.org') &nbsp;OpenSubtitles&nbsp;
+      a.js-external-link(href='http://www.opensubtitles.org') &nbsp;OpenSubtitles&nbsp;
       | in a very fast way.
     h5 Why?
     p Hi, my name is
-      a(href='https://tucci.me') &nbsp;Mati Tucci&nbsp;
+      a.js-external-link(href='https://tucci.me') &nbsp;Mati Tucci&nbsp;
       | and I was using a similar app that recently stopped being free.
       | So, that was a good excuse to spend a few hours building my own free app.
     .info-footer
-      a(href='https://github.com/matiastucci/easysubs')
+      a.js-external-link(href='https://github.com/matiastucci/easysubs')
         span.icon.icon-github
-      a(href='https://twitter.com/TucciMatias')
+      a.js-external-link(href='https://twitter.com/TucciMatias')
         span.icon.icon-twitter
 </template>
 
